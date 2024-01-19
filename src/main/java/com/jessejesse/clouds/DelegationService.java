2e71d4ddc7a132965dc173d3a9fb586cbe791e70
Sudo-Self <sudo-self@proton.me>
2024-01-19 07:07:27 -0700

    Add files via upload

---
+++ src/main/java/com/jessejesse/clouds/DelegationService.java
@@ -0,0 +1,14 @@
+package com.jessejesse.clouds;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
